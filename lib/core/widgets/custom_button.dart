509aec3172468cf8c06d8b7c7cdf346059788551
saniw <aswin@wonderlabs.io>
2020-08-07 14:56:13 +0700

    Added Custom Button

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  CustomButton({@required this.onPressed});
+  CustomButton({@required this.onPressed, @required this.title});
+
   final GestureTapCallback onPressed;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
+      child: Center(
+        child: Text(title),
+      ),
       fillColor: Colors.lightGreen,
       splashColor: Colors.greenAccent,
       onPressed: this.onPressed,
     );
   }
-
-
 }
